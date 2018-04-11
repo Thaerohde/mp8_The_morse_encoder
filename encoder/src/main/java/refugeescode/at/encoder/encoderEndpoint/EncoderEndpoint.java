7c702dfac306f1734e6044c742b1308aa8e71b37
Thaerohde <thaerohde@gmail.com>
2018-04-12 01:01:27 +0200

    add spring files

---
+++ encoder/src/main/java/refugeescode/at/encoder/encoderEndpoint/EncoderEndpoint.java
@@ -0,0 +1,15 @@
+package refugeescode.at.encoder.encoderEndpoint;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class EncoderEndpoint {
+
+    @GetMapping
+    String
+
+}
---
+++ morse/src/main/java/refugeescode/at/morse/MorseApplication.java
@@ -0,0 +1,12 @@
+package refugeescode.at.morse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorseApplication.class, args);
+	}
+}
