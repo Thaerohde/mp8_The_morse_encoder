3fae212c2fb7893fcaeacad254e10b82bcadd0fb
Thaer Ohde <thaerohde@Thaers-MacBook-Pro.local>
2018-04-13 15:17:05 +0200

    finish

--- encoder/src/main/java/refugeescode/at/encoder/encoderEndpoint/EncoderEndpoint.java
+++ encoder/src/main/java/refugeescode/at/encoder/encoderEndpoint/EncoderEndpoint.java
@@ -17,26 +17,18 @@ public class EncoderEndpoint {
     @Value("${morseUrl}")
     private String morseUrl;
 
-    private String text;
     private RestTemplate restTemplate = new RestTemplate();
 
 
-    @GetMapping
-    String addTextToMorse(@RequestBody String toMorse) {
-
-        return Stream.of(toMorse)
-                .map(letter -> connectWithMorse(letter))
-                .collect(Collectors.toList())
-                .toString();
-
+    @PostMapping
+    String addTextToMorse(@RequestBody String message) {
+        return Stream.of(message.split(""))
+                .map(letter -> toMorse(letter))
+                .collect(Collectors.joining());
     }
 
-    private String connectWithMorse(String letter) {
-        String cipherString = "";
-        restTemplate.postForEntity(morseUrl, letter, String.class);
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(morseUrl, String.class);
-        cipherString = cipherString + responseEntity.getBody();
-        return cipherString;
+    private String toMorse(String letter) {
+        return restTemplate.postForObject(morseUrl, letter, String.class);
     }
 
 }
--- morse/src/main/java/refugeescode/at/morse/morseEndpoint/MorseEndpoint.java
+++ morse/src/main/java/refugeescode/at/morse/morseEndpoint/MorseEndpoint.java
@@ -1,45 +1,37 @@
 package refugeescode.at.morse.morseEndpoint;
 
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@RequestMapping("/morse")
+
 @RestController
+@RequestMapping
 public class MorseEndpoint {
 
-    String morseString = "";
+    private List<String> letters = Stream.of("a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" "))
+            .collect(Collectors.toList());
+
+    private List<String> morse = Stream.of("  /.- / -... / -.-. / -.. / . / ..-. / --. / .... / .. / .--- / -.- / .-.. / -- / -. / --- / .--. / --.- / .-. / ... / - / ..- / ...- / .-- / -..- / -.-- / --..".split(" / "))
+            .collect(Collectors.toList());
 
     @PostMapping("/morse")
-    String chipher(@RequestBody String message) {
-        morseString = morseString + toMorse(message);
-        return morseString;
+    String chipher(@RequestBody String letter) {
+        if(letter.trim().isEmpty()){
+            return " ";
+        }
+        else return toMorse(letter);
     }
 
-    @GetMapping("/morse")
-    String get() {
-        String getMappingString = "";
-        getMappingString = getMappingString + morseString;
-        return getMappingString.replaceAll("\"", "").trim();
-    }
-
-    public String toMorse(String input) {
-        List<String> letters = Stream.of("a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" "))
-                .collect(Collectors.toList());
-
-        List<String> morse = Stream.of(".- / -... / -.-. / -.. / . / ..-. / --. / .... / .. / .--- / -.- / .-.. / -- / -. / --- / .--. / --.- / .-. / ... / - / ..- / ...- / .-- / -..- / -.-- / --..".split(" / "))
-                .collect(Collectors.toList());
-
-        String collect = Stream.of(input).map(e -> input.indexOf(e))
-                .map(e1 -> morse.indexOf(e1))
-                .collect(Collectors.toList())
-                .toString();
-
-
-        return collect;
+    public String toMorse(String letter) {
+        int index = letters.indexOf(letter);
+        return morse.get(index);
 
     }
 }
